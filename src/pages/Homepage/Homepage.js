36e7fe8b6bc2938e596b576908e9fbfe355ef160
Mark De Guzman <markdeguzman1142@gmail.com>
2022-04-02 03:22:51 -0700

    Fixed Quote api fetch on homepage

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,6 +1,6 @@
 import { createTheme, ThemeProvider, Typography, Box } from "@mui/material";
 import Heading from "../../components/Navbar";
-const axios = require('axios')
+import axios from "axios";
 
 const theme = createTheme({
     components: {
@@ -27,25 +27,17 @@ const theme = createTheme({
         }
     }
 });
-const config = {
-    headers: {'Access-Control-Allow-Origin': '*'}
-};
-function yourFunction(){
-    // do whatever you like here
-    // axios.get('https://zenquotes.io/api/random', { crossdomain: true }).then(response => {
-    //     console.log(response)
-    // })
 
-    const fetchData = async () => {
-        const result = await fetch(`https://zenquotes.io/api/random`);
-        const body = await result.json();
-        console.log(body)
-        }
-    fetchData();
-
-    // setTimeout(yourFunction, 5000);
+function getQuote(){
+    axios.get('/api/random').then(response => {
+        console.log(response.data[0].q)
+        console.log(response.data[0].a)
+        document.getElementsByClassName('quote')[0].innerHTML = response.data[0].q
+        document.getElementsByClassName('author')[0].innerHTML = response.data[0].a
+    })
+    setTimeout(getQuote, 5000);
 }
-yourFunction();
+getQuote();
 
 function Homepage() {
 
@@ -58,7 +50,9 @@ function Homepage() {
                         <div style={{margin: 'auto'}}>
                             <Typography variant="name">MARK DE GUZMAN</Typography>
                             <br />
-                            <Typography className="quote" variant="quote">(Quote)</Typography>
+                            <Typography className="quote" variant="quote"></Typography>
+                            <br />
+                            <Typography className="author" variant="quote"></Typography>
                         </div>
                     </div>
                     <div style={{ height: '100%' }}>
@@ -71,9 +65,6 @@ function Homepage() {
                         <Typography variant="quote">(Quote)</Typography>
                     </div>
                 </Box>
-                <div>
-                    eeeee
-                </div>
             </ThemeProvider>
         </>
     )
