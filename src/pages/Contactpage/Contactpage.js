c845ba5d4a7757d0f1b7782511103b0e19d5b04f
Mark De Guzman <markdeguzman1142@gmail.com>
2022-04-14 21:03:47 -0700

    added workflow

--- src/pages/Contactpage/Contactpage.js
+++ src/pages/Contactpage/Contactpage.js
@@ -9,17 +9,23 @@ export default function Contactpage() {
     const message = useRef('');
 
     function sendMessage() {
-        console.log(email.current)
-        console.log(name.current)
-        console.log(message.current)
-        if (email.current == '' || name.current == '' || message.current == '') {
+        console.log(email.current.value)
+        console.log(name.current.value)
+        console.log(message.current.value)
+        if (email.current.value == '' || name.current.value == '' || message.current.value == '') {
             alert('Please fill out all fields')
         } else {
-            email.current = ''
-            name.current = ''
-            message.current = ''
+            email.current.value = ''
+            name.current.value = ''
+            message.current.value = ''
             alert('Message submitted')
         }
+    }
+
+    function clearMessage() {
+        email.current.value = ''
+        name.current.value = ''
+        message.current.value = ''
     }
 
     return (
@@ -33,17 +39,17 @@ export default function Contactpage() {
                     </Fade>
                     <div style={{ textAlign: 'left' }}>
                         <Fade left>
-                            <TextField id="outlined-basic" ref={email} label="Email" variant="outlined" color="basic" sx={{ width: '40%', my: '3vh' }} inputProps={{ style: { color: "white" } }} focused />
+                            <TextField id="outlined-basic" inputRef={email} label="Email" variant="outlined" color="basic" sx={{ width: '40%', my: '3vh' }} inputProps={{ style: { color: "white" } }} focused />
                             <br />
-                            <TextField id="outlined-basic" ref={name} label="Name" variant="outlined" color="basic" sx={{ width: '40%', my: '3vh' }} inputProps={{ style: { color: "white" } }} focused />
+                            <TextField id="outlined-basic" inputRef={name} label="Name" variant="outlined" color="basic" sx={{ width: '40%', my: '3vh' }} inputProps={{ style: { color: "white" } }} focused />
                         </Fade>
                     </div>
                     <Fade right>
-                        <TextField id="outlined-basic" ref={message} label="Message" variant="outlined" color="basic" multiline={true} rows={8} sx={{ width: '100%', my: '5vh' }} inputProps={{ style: { color: "white" } }} focused />
+                        <TextField id="outlined-basic" inputRef={message} label="Message" variant="outlined" color="basic" multiline={true} rows={8} sx={{ width: '100%', my: '5vh' }} inputProps={{ style: { color: "white" } }} focused />
                     </Fade>
                     <Fade bottom>
                         <div style={{ textAlign: 'left' }}>
-                            <Button variant="outlined" color="error" style={{ textTransform: 'none', width: '10em', margin: '2em 2em 2em 0' }}>
+                            <Button variant="outlined" color="error" style={{ textTransform: 'none', width: '10em', margin: '2em 2em 2em 0' }} onClick={() => { clearMessage() }}>
                                 <Typography id="quote" className="quote" variant="info">Clear</Typography>
                             </Button>
                             <Button variant="outlined" color="basic" style={{ textTransform: 'none', width: '10em', margin: '2em 2em 2em 0' }} onClick={() => { sendMessage() }}>
