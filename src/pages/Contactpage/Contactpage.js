aac99027cae1f879d17cc9279d86b0a3ba40cb93
mark-p7 <markdeguzman1142@gmail.com>
2022-04-15 18:50:28 -0700

    new contact page

--- src/pages/Contactpage/Contactpage.js
+++ src/pages/Contactpage/Contactpage.js
@@ -7,11 +7,11 @@ export default function Contactpage() {
         <>
             <div id="Contact">
                 <Box sx={{ backgroundColor: 'white', width: '100%', height: '30em' }}>
-                <FormControl>
-                    <InputLabel htmlFor="my-input">Email address</InputLabel>
-                    <Input id="my-input" aria-describedby="my-helper-text" />
-                    <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
-                </FormControl>
+                    <FormControl>
+                        <InputLabel htmlFor="my-input">Email address</InputLabel>
+                        <Input id="my-input" aria-describedby="my-helper-text" />
+                        <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+                    </FormControl>
                 </Box>
             </div>
         </>
